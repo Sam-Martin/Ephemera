c17da28052fc769bd81de9344d5a50b840fc2591
Sam Martin <samjackmartin@gmail.com>
2015-09-17 23:45:03 +0100

    Initial terraform commit

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "access_key" {}
+variable "secret_key" {}
+variable "region" {default = "eu-west-1"}
+
+
+variable "private_bucket_name" {}
+variable "public_bucket_name" {}
+variable "ephemera_s3_url_signer" {default = "EphemeraS3Signer"}
+variable "lambda_role_name" {default = "EphemeraLambda"}
+variable "lambda_executor_role_name" {default = "EphemeraLambdaExecutor"}
+variable "ephemera_lambda_zip" {default = "D:\Dropbox\Dropbox\code\sites\password-dissemination\ephemera.zip"}
