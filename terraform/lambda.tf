fe204999984cca4ce971ab3ca80ed014c0c63ed1
Sam Martin <samjackmartin@gmail.com>
2015-11-01 18:59:42 +0000

    Revert "Brought swagger spec into terraform"
    This reverts commit 5029cb982810074615f0c361dbd6e0f22795a29b.

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,20 +1,20 @@
 resource "aws_lambda_function" "ephemera-getsignedurl" {
     filename = "${var.ephemera_lambda_zip}"
-    function_name = "${var.ephemera_getsignedurl_function_name}"
+    function_name = "ephemera-getsignedurl"
     role = "${aws_iam_role.ephemera_lambda_role.arn}"
     handler = "lambda/ephemera-getsignedurl/ephemera-getsignedurl.handler"
 }
 
 resource "aws_lambda_function" "ephemera-addtextsecret" {
     filename = "${var.ephemera_lambda_zip}"
-    function_name = "${var.ephemera_addtextsecret_function_name}"
+    function_name = "ephemera-addtextsecret"
     role = "${aws_iam_role.ephemera_lambda_role.arn}"
     handler = "lambda/ephemera-addtextsecret/ephemera-addtextsecret.handler"
 }
 
 resource "aws_lambda_function" "ephemera-getsecret" {
     filename = "${var.ephemera_lambda_zip}"
-    function_name = "${var.ephemera_getsecret_function_name}"
+    function_name = "ephemera-getsecret"
     role = "${aws_iam_role.ephemera_lambda_role.arn}"
     handler = "lambda/ephemera-getsecret/ephemera-getsecret.handler"
 }
