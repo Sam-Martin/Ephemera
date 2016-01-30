4e0a9ebb620d63cd8cdfb6799d1ab05de3131962
Sam Martin <samjackmartin@gmail.com>
2016-01-30 16:17:16 +0000

    Updated test config to (hopefully) be valid

--- lambda/common/ephemera-config.js
+++ lambda/common/ephemera-config.js
@@ -1,11 +1,11 @@
 exports.config = function () {
   return {
-    accessKeyId: 'AKIAJHGIKFRVI5CXTVVQ',
+    accessKeyId: 'AKIAILPS6G7S6YQ4AUMA',
     // Name of the bucket to upload to
     bucketName: 'smartin-test-beta-private',
     bucketRegion: 'eu-west-1',
     successActionRedirect: 'http://smartin-test-beta-public.s3-website-eu-west-1.amazonaws.com',
     s3ACL: 'private',
-    encryptedSecret: 'CiCV2KPOX2hYFgn4cGOI8oD2tzSjWrkekeZwn624FdeMGhKwAQEBAgB4ldijzl9oWBYJ+HBjiPKA9rc0o1q5HpHmcJ+tuBXXjBoAAACHMIGEBgkqhkiG9w0BBwagdzB1AgEAMHAGCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMf6hEcACOY1YFlCr3AgEQgENaE6LkA63filppZ+lLo6KNNDcSO4nGVgUqkPrTDA9uztZCAZrRanG+bg+o18EXAdnPNzpd6pWgzAEfSpWP5UJ76lIb'
+    encryptedSecret: 'CiCGkuKeBRDpoYXzr/AFCoCS8ocdC/lnJT/R8TGgWdAebxKwAQEBAgB4hpLingUQ6aGF86/wBQqAkvKHHQv5ZyU/0fExoFnQHm8AAACHMIGEBgkqhkiG9w0BBwagdzB1AgEAMHAGCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMuB7CXHHsLxXDM27yAgEQgEPYa24EcHfaizhsXKedjj/iFwZsRf2QP8/UJEERPjR+sAsJWQmZh4bXJ1Pp0WRTBD+kbuawyRpadzfwL2EDgayIjbtP'
   };
 };
