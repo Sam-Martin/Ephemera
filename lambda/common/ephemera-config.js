e072f9a7fb9869ccfcc3856d2e33f92cd98f41dd
Sam Martin <samjackmartin@gmail.com>
2016-01-30 17:44:42 +0000

    Hmm, issue needs to be resolved whereby terraform and tests use (and modify) the same file

--- lambda/common/ephemera-config.js
+++ lambda/common/ephemera-config.js
@@ -1,11 +1,11 @@
 exports.config = function () {
   return {
-    accessKeyId: 'AKIAJE7HMRR77HE5FQ6A',
+    accessKeyId: 'AKIAILPS6G7S6YQ4AUMA',
     // Name of the bucket to upload to
     bucketName: 'smartin-test-beta-private',
     bucketRegion: 'eu-west-1',
     successActionRedirect: 'http://smartin-test-beta-public.s3-website-eu-west-1.amazonaws.com',
     s3ACL: 'private',
-    encryptedSecret: 'CiCV2KPOX2hYFgn4cGOI8oD2tzSjWrkekeZwn624FdeMGhKwAQEBAgB4ldijzl9oWBYJ+HBjiPKA9rc0o1q5HpHmcJ+tuBXXjBoAAACHMIGEBgkqhkiG9w0BBwagdzB1AgEAMHAGCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMvCpwEzJr1DRH6D3+AgEQgEOHES69s/sZuYhDciVodbr9xuohsNA6RkUJgZJHQyEsltou+7HXtf3yyt53Xa/U96h9MAogUM/dsCtwUL0iMgYQ/0B0'
+    encryptedSecret: 'CiCV2KPOX2hYFgn4cGOI8oD2tzSjWrkekeZwn624FdeMGhKwAQEBAgB4ldijzl9oWBYJ+HBjiPKA9rc0o1q5HpHmcJ+tuBXXjBoAAACHMIGEBgkqhkiG9w0BBwagdzB1AgEAMHAGCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMLWekeLEUKFuSvYm1AgEQgENc6Ue9oJZqHQgZo3EL7cJiyuKjOYH+W5PIDhv1nGCR23w6xFYOtKXUaVcHvijGRh8GobE5Di5FcQ7PAhqvtJFgahYW'
   };
 };
