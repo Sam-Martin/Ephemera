ad29dfdb7602f0a2dc571a81c7f3bb3a7c51d3df
Sam Martin <samjackmartin@gmail.com>
2018-12-09 20:51:02 +0000

    Refactor

--- lambda/ephemera-addtextsecret/ephemera-addtextsecret.js
+++ lambda/ephemera-addtextsecret/ephemera-addtextsecret.js
@@ -5,49 +5,55 @@ AWS.config.update({
   endpoint: "https://dynamodb."+process.env.REGION+".amazonaws.com"
 });
 var docClient = new AWS.DynamoDB.DocumentClient();
-
+var bucketKey
 
 exports.handler = function (event, context,callback) {
   // Generate a UUID for a key
-  var bucketKey = generateUUID();
-  encrypt(new Buffer(JSON.parse(event.body).secretText,'utf-8')).then(base64EncryptedString => {
-    var params = {
-      TableName: process.env.DYNAMODB_TABLE_NAME,
-      Item: {
-          SecretID: bucketKey,
-          SecretText: base64EncryptedString,
-          Uploaded: new Date().getTime()
-      }
-    };
-
-    // Save the text to DynamoDB
-    docClient.put(params, function(err, data) {
-        if (err) {
-          return callback(null, {
-            headers: {
-              "Access-Control-Allow-Origin" : "*"
-            },
-            statusCode: 500,
-            isBase64Encoded: false,
-            body: JSON.stringify({message: "Failed to insert record to database"})
-          });
-
-        } else {
-              console.log("Successfully put " + bucketKey + " into " + process.env.DYNAMODB_TABLE_NAME);
-              callback(null, {
-                headers: {
-                  "Access-Control-Allow-Origin" : "*"
-                },
-                statusCode: 200,
-                isBase64Encoded: false,
-                body: JSON.stringify({key: bucketKey})
-              });
-        }
+  bucketKey = generateUUID();
+  encrypt(new Buffer(JSON.parse(event.body).secretText,'utf-8')).then(saveSecret).catch(err => {
+    return callback(null, {
+      headers: {
+        "Access-Control-Allow-Origin" : "*"
+      },
+      statusCode: 500,
+      isBase64Encoded: false,
+      body: JSON.stringify({message: "Failed to insert record to database"})
     });
-  })
-
+  }).then(returnSecret(callback))
 };
 
+function returnSecret(callback){
+      console.log("Successfully put " + bucketKey + " into " + process.env.DYNAMODB_TABLE_NAME);
+      callback(null, {
+        headers: {
+          "Access-Control-Allow-Origin" : "*"
+        },
+        statusCode: 200,
+        isBase64Encoded: false,
+        body: JSON.stringify({key: bucketKey})
+      });
+}
+
+function saveSecret(base64EncryptedString) {
+  var params = {
+    TableName: process.env.DYNAMODB_TABLE_NAME,
+    Item: {
+        SecretID: bucketKey,
+        SecretText: base64EncryptedString,
+        Uploaded: new Date().getTime()
+    }
+  };
+  return new Promise((resolve, reject) => {
+    // Save the text to DynamoDB
+    docClient.put(params, function(err, data) {
+        if (err){
+          reject(err)
+        } else {
+          resolve(data)
+        }
+    })
+  })
+}
 
 function encrypt(buffer) {
     const kms = new AWS.KMS({
