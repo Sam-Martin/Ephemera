191223efc03f0ea01424bdfadf318609b70f1d59
Sam Martin <samjackmartin@gmail.com>
2018-12-10 11:38:00 +0000

    Refactor

--- lambda/ephemera-ageoffsecret/ephemera-ageoffsecret.js
+++ lambda/ephemera-ageoffsecret/ephemera-ageoffsecret.js
@@ -1,43 +1,61 @@
 console.log('loading event');
 var AWS = require('aws-sdk');
 AWS.config.update({
-  region: process.env.REGION,
-  endpoint: "https://dynamodb."+process.env.REGION+".amazonaws.com"
+    region: process.env.REGION,
+    endpoint: "https://dynamodb." + process.env.REGION + ".amazonaws.com"
 });
 var docClient = new AWS.DynamoDB.DocumentClient();
 
-exports.handler = function (event, context, callback) {
-  console.log('Loaded handler');
+exports.handler = function(event, context, callback) {
+    console.log('Loaded handler');
 
-  var now = new Date();
-  var params = {
-      TableName: process.env.DYNAMODB_TABLE_NAME,
-      FilterExpression:"Uploaded <= :uploaded",
-      ExpressionAttributeValues: {
-        ":uploaded": now.setHours(now.getHours() - process.env.MAX_SECRET_AGE_HOURS)
-      }
-  };
-
-  docClient.scan(params, function(err, data) {
-    if (err) {
+    getOldSecrets().catch(err => {
         console.error("Unable to search for expired items. Error JSON:", JSON.stringify(err, null, 2));
-    }
-    data.Items.forEach(function (item) {
-      params = {
-         TableName: process.env.DYNAMODB_TABLE_NAME,
-         Key: {
-           "SecretID": item.SecretID
-          }
-      }
-      docClient.delete(params, function(err, data) {
-         if (err) {
-          console.error("Unable to delete item. Error JSON:", JSON.stringify(err, null, 2));
-          return
-         }
-         console.log(data)
-      })
+    }).then(data => {
+        data.Items.forEach(function(item) {
+            deleteSecret(item).catch(err => {
+                console.error("Unable to delete item. Error JSON:", JSON.stringify(err, null, 2));
+            }).then(item => {
+                console.log("Successfully aged off: " + item.SecretID)
+            })
+        })
     })
-    console.log(data)
-  })
+}
 
+function getOldSecrets() {
+    var now = new Date();
+    var params = {
+        TableName: process.env.DYNAMODB_TABLE_NAME,
+        FilterExpression: "Uploaded <= :uploaded",
+        ExpressionAttributeValues: {
+            ":uploaded": now.setHours(now.getHours() - process.env.MAX_SECRET_AGE_HOURS)
+        }
+    };
+    return new Promise((resolve, reject) => {
+        docClient.scan(params, function(err, data) {
+            if (err) {
+                reject(err)
+            } else {
+                resolve(data)
+            }
+        })
+    })
+}
+
+function deleteSecret(item) {
+    var params = {
+        TableName: process.env.DYNAMODB_TABLE_NAME,
+        Key: {
+            "SecretID": item.SecretID
+        }
+    }
+    return new Promise((resolve, reject) => {
+        docClient.delete(params, function(err, data) {
+            if (err) {
+                reject(err)
+            } else {
+                resolve(item)
+            }
+        })
+    })
 }
